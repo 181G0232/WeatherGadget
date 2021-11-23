96d22163842a326db9119177b0727b732360be91
Juan Jesús Chávez Villa <janitochvvll2311@gmail.com>
2021-11-23 09:45:22 -0600

    Refactory

--- WeatherGadget.ino
+++ WeatherGadget.ino
@@ -1,28 +1,148 @@
-#include "Arduino.h"
-#include "Roselino/Digital.hpp"
+#include <Arduino.h>
+#include <WiFi.h>
 
-Digital input(2, INPUT);
-Digital out_state(3, OUTPUT);
-Digital out_rising(4, OUTPUT);
-Digital out_falling(5, OUTPUT);
+const char *ap_ssid = "INFINTUM_32";
+const char *ap_pass = "mod32";
 
-bool rising_state = false;
-bool falling_state = false;
+WiFiServer server(80);
+
+void connectWiFi(const char *ssid, const char *pass)
+{
+    Serial.print("Connecting to WiFi... ");
+    WiFi.begin(ssid, pass);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+    }
+    Serial.println("Done");
+    Serial.print("WiFi IP address: ");
+    Serial.println(WiFi.localIP());
+}
+
+void OnWiFiEvent(WiFiEvent_t event)
+{
+    switch (event)
+    {
+    case SYSTEM_EVENT_STA_CONNECTED:
+        Serial.println("ESP32 connected to WiFi Network");
+        break;
+    case SYSTEM_EVENT_STA_DISCONNECTED:
+        Serial.println("ESP32 cisconnected to WiFi Network");
+        break;
+    case SYSTEM_EVENT_AP_STACONNECTED:
+        Serial.println("Station connected to ESP32 soft AP");
+        break;
+    case SYSTEM_EVENT_AP_STADISCONNECTED:
+        Serial.println("Station disconnected from ESP32 soft AP");
+        break;
+    default:
+        break;
+    }
+}
 
 void setup()
 {
-    /* Do nothing */
+    Serial.begin(115200);
+    WiFi.onEvent(OnWiFiEvent);
+
+    Serial.println("Iniciando AP");
+    WiFi.mode(WIFI_MODE_APSTA);
+    WiFi.softAP(ap_ssid, ap_pass);
+
+    Serial.print("AP IP address: ");
+    Serial.println(WiFi.softAPIP());
 }
 
 void loop()
 {
-    out_state.write(input.read());
-    if (input.rising())
-    {
-        out_rising.write(rising_state = !rising_state);
-    }
-    if (input.falling())
-    {
-        out_falling.write(falling_state = !falling_state);
+    WiFiClient client = server.available(); // Listen for incoming clients
+
+    if (client)
+    {                                  // If a new client connects,
+        Serial.println("New Client."); // print a message out in the serial port
+        String currentLine = "";       // make a String to hold incoming data from the client
+        String header = "";
+        while (client.connected())
+        { // loop while the client's connected
+            if (client.available())
+            {                           // if there's bytes to read from the client,
+                char c = client.read(); // read a byte, then
+                Serial.write(c);        // print it out the serial monitor
+                header += c;
+                if (c == '\n')
+                { // if the byte is a newline character
+                    // if the current line is blank, you got two newline characters in a row.
+                    // that's the end of the client HTTP request, so send a response:
+                    if (currentLine.length() == 0)
+                    {
+                        // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+                        // and a content-type so the client knows what's coming, then a blank line:
+                        client.println("HTTP/1.1 200 OK");
+                        client.println("Content-type:text/html");
+                        client.println("Connection: close");
+                        client.println();
+
+                        // turns the GPIOs on and off
+                        if (header.indexOf("GET /26/on") >= 0)
+                        {
+                            Serial.println("GPIO 26 on");
+                        }
+                        else if (header.indexOf("GET /26/off") >= 0)
+                        {
+                            Serial.println("GPIO 26 off");
+                        }
+                        else if (header.indexOf("GET /27/on") >= 0)
+                        {
+                            Serial.println("GPIO 27 on");
+                        }
+                        else if (header.indexOf("GET /27/off") >= 0)
+                        {
+                            Serial.println("GPIO 27 off");
+                        }
+
+                        // Display the HTML web page
+                        client.println("<!DOCTYPE html><html>");
+                        client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+                        client.println("<link rel=\"icon\" href=\"data:,\">");
+                        // CSS to style the on/off buttons
+                        // Feel free to change the background-color and font-size attributes to fit your preferences
+                        client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+                        client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
+                        client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+                        client.println(".button2 {background-color: #555555;}</style></head>");
+
+                        // Web Page Heading
+                        client.println("<body><h1>ESP32 Web Server</h1>");
+
+                        // Display current state, and ON/OFF buttons for GPIO 26
+                        client.println("<p>GPIO 26 - State (DISCARTED BY CHAVEZ) </p>");
+
+                        // Display current state, and ON/OFF buttons for GPIO 27
+                        client.println("<p>GPIO 27 - State (DISCARTED BY CHAVEZ) </p>");
+
+                        client.println("</body></html>");
+
+                        // The HTTP response ends with another blank line
+                        client.println();
+                        // Break out of the while loop
+                        break;
+                    }
+                    else
+                    { // if you got a newline, then clear currentLine
+                        currentLine = "";
+                    }
+                }
+                else if (c != '\r')
+                {                     // if you got anything else but a carriage return character,
+                    currentLine += c; // add it to the end of the currentLine
+                }
+            }
+        }
+        // Clear the header variable
+        header = "";
+        // Close the connection
+        client.stop();
+        Serial.println("Client disconnected.");
+        Serial.println("");
     }
 }
